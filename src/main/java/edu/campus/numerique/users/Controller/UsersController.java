144366a07dfd058515068f591689192608c9c7e5
Lucas Gratien <lucas.gratien@le-campus-numerique.fr>
2024-11-04 14:54:40 +0100

    maj

--- src/main/java/edu/campus/numerique/users/Controller/UsersController.java
+++ src/main/java/edu/campus/numerique/users/Controller/UsersController.java
@@ -1,9 +1,11 @@
 package edu.campus.numerique.users.Controller;
+
 import edu.campus.numerique.users.model.UserNotFound;
 import edu.campus.numerique.users.model.Users;
 import edu.campus.numerique.users.service.UsersService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -22,11 +24,17 @@ public class UsersController {
     public UsersController(UsersService usersService) {
         this.usersService = usersService;
     }
+
     @Operation(summary = "Récupération de tous les users")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Opération reussie",
-            content = @Content(mediaType = "application/json",
-            schema = @Schema(implementation = Users.class))),
+            @ApiResponse(responseCode = "200", description = "Opération réussie",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Users.class),
+                            examples = @ExampleObject(
+                                    name = "Exemple d'utilisateur",
+                                    value = "[{\"id\": 1, \"name\": \"Lucas Gratien\", \"email\": \"lucas.gratien@example.com\", \"age\": 25}]"
+                            )
+                    )),
             @ApiResponse(responseCode = "400", description = "Requête invalide",
                     content = @Content(mediaType = "application/json")),
             @ApiResponse(responseCode = "404", description = "Client non trouvé",
@@ -36,10 +44,15 @@ public class UsersController {
     public List<Users> getAllUsers() {
         return usersService.findAll();
     }
+
     @Operation(summary = "Récupère un client par son ID")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Client trouvé",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Users.class))),
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Users.class),
+                            examples = @ExampleObject(
+                                    value = "{\"id\": 1, \"name\": \"Lucas gratien\", \"email\": \"lucas.gratien@example.com\", \"age\": 25}"
+                            ))),
             @ApiResponse(responseCode = "404", description = "Client non trouvé",
                     content = @Content(mediaType = "application/json"))
     })
@@ -47,10 +60,15 @@ public class UsersController {
     public Users getUserById(@PathVariable int id) {
         return usersService.findById(id).orElseThrow(UserNotFound::new);
     }
+
     @Operation(summary = "Ajoute un nouveau client")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Client créé avec succès",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Users.class))),
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Users.class),
+                            examples = @ExampleObject(
+                                    value = "{\"id\": 2, \"name\": \"Lulu gratien\", \"email\": \"lulu.gratien@example.com\", \"age\": 30}"
+                            ))),
             @ApiResponse(responseCode = "400", description = "Requête invalide",
                     content = @Content(mediaType = "application/json"))
     })
@@ -58,10 +76,15 @@ public class UsersController {
     public Users createUser(@RequestBody Users users) {
         return usersService.save(users);
     }
+
     @Operation(summary = "Met à jour un client existant")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Client mis à jour avec succès",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Users.class))),
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Users.class),
+                            examples = @ExampleObject(
+                                    value = "{\"id\": 1, \"name\": \"Lucas Gratien\", \"email\": \"lucas.gratien@example.com\", \"age\": 26}"
+                            ))),
             @ApiResponse(responseCode = "404", description = "Client non trouvé",
                     content = @Content(mediaType = "application/json")),
             @ApiResponse(responseCode = "400", description = "Requête invalide",
@@ -71,6 +94,7 @@ public class UsersController {
     public ResponseEntity<Users> updateUser(@PathVariable int id, @RequestBody Users users) {
         return ResponseEntity.ok(usersService.update(id, users));
     }
+
     @Operation(summary = "Supprime un client par son ID")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "204", description = "Client supprimé avec succès"),
--- src/main/java/edu/campus/numerique/users/model/Users.java
+++ src/main/java/edu/campus/numerique/users/model/Users.java
@@ -1,13 +1,13 @@
 package edu.campus.numerique.users.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.time.LocalDate;
-import java.time.Period;
 
 
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor
@@ -15,13 +15,13 @@ import java.time.Period;
 @Entity
 public class Users {
     @Id
-   @GeneratedValue
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String firstname;
     private String lastname;
     private LocalDate dateofbirth;
     private String numlicense;
     private LocalDate datelicense;
-
 }
+
 
